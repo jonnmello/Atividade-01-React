fa64d8367102b8d47a20ccadac8eb05ead940e8f
Jonatas Melo <jonnmello@gmail.com>
2022-05-18 17:13:52 -0300

    Atual

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -17,7 +17,6 @@ const Home = () => {
           <div className="home-content-container">
             <h1> O carro perfeito para você</h1>
             <p>
-              {' '}
               Conheça nossos carros e dê mais um passo na realização do seu
               sonho
             </p>
