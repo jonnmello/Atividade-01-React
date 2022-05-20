7f041eff9d052d61002923e114cbe83a92b96ce0
Jonatas Melo <jonnmello@gmail.com>
2022-05-20 16:07:22 -0300

    Final

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,7 +1,17 @@
+import ButtomSearch from 'components/ButtomSearch';
 import ProductCard from 'components/ProductCard';
+import './styles.css';
 
 const Catalog = () => {
   return (
+
+    <div className="catalog-container">
+    <div>
+      <div className="base-base2 catalog-content">
+        <ButtomSearch/>
+        
+      </div>
+
       <div className="container my-4">
         <div className="row">
           <div className="col-sm-6 col-lg-4">
@@ -23,7 +33,8 @@ const Catalog = () => {
           <ProductCard/>
           </div>
         </div>
-        
+        </div>
+      </div>
       </div>
     
   );
